ff57695cc072911e0d6042bdb57cbe9851531f9b
Hannes <hannes.porombka@gmail.com>
2023-04-06 00:48:11 +0200

    created Cupcake Model and ran seed.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 """Flask app for Cupcakes"""
+from flask import Flask, render_template, request, redirect
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+
+connect_db(app)
+
+with app.app_context():
+    db.create_all()
--- models.py
+++ models.py
@@ -1,1 +1,29 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect to database"""
+
+    db.app = app
+    db.init_app(app)
+
+
+class Cupcake (db.Model):
+    """Cupcake"""
+
+    ___tablename___ = "cupcakes"
+
+    def __repr__(self):
+        """ Cupcake Info"""
+        c = self
+        return (f"Cupcake {c.id}, flavor: {c.flavor}, size: {c.size}")
+
+    id = db.Column(db.Integer, primary_key = True,
+                   autoincrement = True)
+    flavor = db.Column (db.String(30))
+    size = db.Column(db.String(30))
+    rating = db.Column(db.Float)
+    image = db.Column(db.Text, default = "https://tinyurl.com/demo-cupcake")
+
