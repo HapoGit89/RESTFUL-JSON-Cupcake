1efe337aa3146886d8bb77b65d355293dc30c812
Hannes <hannes.porombka@gmail.com>
2023-04-06 01:27:35 +0200

    POST route working

--- app.py
+++ app.py
@@ -29,8 +29,36 @@ def serialize_cupcake(cupcake):
 
 
 @app.route("/api/cupcakes", methods = ["GET"])
+
 def list_cupcakes():
+    """Shows List of all cakes"""
     cakes = Cupcake.query.all()
     serialized_cupcakes = [serialize_cupcake(cake) for cake in cakes]
-    return jsonify(cupcakes = serialized_cupcakes)
+    return (jsonify(cupcakes = serialized_cupcakes),200)
+
+@app.route("/api/cupcakes/<id>")
+def list_cupcake(id):
+    """ Shows Cupcake of id id"""
+    cake = Cupcake.query.get_or_404(id)
+    cake_serialized = serialize_cupcake(cake)
+    return (jsonify(cupcake = cake_serialized),200)
+
+
+@app.route("/api/cupcakes", methods = ["POST"])
+def create_cupcake():
+    """creates cupcake"""
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    try:
+        image = request.json["image"]
+    except:
+        image = "https://images.eatsmarter.de/sites/default/files/styles/max_size/public/cupcake-mit-rosa-creme-435108.jpg"
+   
+    cake = Cupcake(flavor = flavor, size = size, rating = rating, image = image)
+    
+    db.session.add(cake)
+    db.session.commit()
+    cake_ser = serialize_cupcake(cake)
+    return (jsonify(cupcake=cake_ser), 201)
 
