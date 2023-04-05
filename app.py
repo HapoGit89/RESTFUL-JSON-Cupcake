36280ad8d918eddd47010d5551bfb58bf9227c9c
Hannes <hannes.porombka@gmail.com>
2023-04-06 00:59:56 +0200

    first route working

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 """Flask app for Cupcakes"""
 from flask import Flask, render_template, request, redirect
 from models import db, connect_db, Cupcake
+from flask.json import jsonify
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
@@ -12,3 +14,23 @@ connect_db(app)
 
 with app.app_context():
     db.create_all()
+
+def serialize_cupcake(cupcake):
+    """Serialize cupcake for json parsing"""
+
+    return {
+        "id": cupcake.id,
+        "flavor": cupcake.flavor,
+        "size": cupcake.size,
+        "rating": cupcake.rating,
+        "image": cupcake.image
+    }
+
+
+
+@app.route("/api/cupcakes", methods = ["GET"])
+def list_cupcakes():
+    cakes = Cupcake.query.all()
+    serialized_cupcakes = [serialize_cupcake(cake) for cake in cakes]
+    return jsonify(cupcakes = serialized_cupcakes)
+
